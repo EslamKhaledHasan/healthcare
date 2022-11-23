dbd9004d1e7ff8c68e9d87ecb000949ec2534910
eslaamm <eslamkhaledhasan2@gmail.com>
2022-11-23 17:49:38 +0200

    some changes

--- health care/patients.cs
+++ health care/patients.cs
@@ -12,9 +12,7 @@ namespace health_care
 {
     public partial class patients : Form
     {
-        functions Con;
-
-
+        Functions Con;
         public patients()
         {
             InitializeComponent();
@@ -23,36 +21,32 @@ namespace health_care
         }
         private void ShowPatients()
         {
-            string Query = "Select from PatientTbl";
-            PatientLst.DataSource = Con.GetData(Query);
-            PatientLst.DataSource ource = PatientLst.DataSource = Con.GetDatCon.GetData(Query);
+            string Query = "select * from patientstbl";
+            PatientsList.DataSource = Con.GetData(Query);
         }
         private void patients_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
+            if(PatNameTb.Text == "" || PatAddTb.Text == "" || GenCb.SelectedIndex == -1)
         {
-
+                MessageBox.Show("Missing Dadta!!");
         }
 
         private void panel3_Paint(object sender, PaintEventArgs e)
         {
-
+                string Patient = PatNameTb.Text;
+                string Gender = GenCb.SelectedItem.ToString();
+                string BDate = DOBTb.Value.Date.ToString();
+                string Phone = PatPhoneTb.Text;
+                string Address = PatAddTb.Text;
+                string Query = "insert into PatientTb1 valuues('{0}','{1}','{2}','{3}','{4}')";
+                Query = string.Format(Query, Patient, Gender, BDate, Phone, Address);
+                Con.SetData(Query);
+                ShowPaints();
+                MessageBox.Show("Patient Adress!!!");
+        }
         }
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label11_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void SaveBtn_Click(object sender, EventArgs e)
+        private void panel3_Paint(object sender, PaintEventArgs e)
         {
 
         }
